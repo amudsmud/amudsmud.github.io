af4c943ab647ef20d14a14bd1c615e922a167e49
amudsmud <31658019+amudsmud@users.noreply.github.com>
2018-05-15 14:35:21 +0200

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -52,7 +52,7 @@ function nyboks(id, ateksten, href, h1teksten, ptekstenover, ptekstenunder) {
 
 function mangebokser() {
     // nyboks(id, ateksten, href, h1teksten, ptekstenover, ptekstenunder)
-    nyboks("b", "Klikk her", "hellerasten-login.html", "for å logge inn på Hellerasten siden", "Hellerasten siden er ikke oppe for øyeblikket. ", "Kommer i løpet av våren 2018")
+    nyboks("b", "Klikk her", "login-side.html", "for å logge inn på Hellerasten siden", "Hellerasten siden er ikke oppe for øyeblikket. ", "Kommer i løpet av våren 2018")
     nyboks("a", "Klikk her", "https://github.com/amudsmud/amudsmud.github.io/releases/download/V0.4.0-beta/Tilfeldig.setegenerator.V0.4.0-beta.exe", "for å laste ned programmet mitt", "Tilfeldig setegenerator®.", "<i> V 0.3.0-beta <br> Sist oppdatert: 24. April 2018 </i>")
     nyboks("a", "Klikk her", "https://github.com/amudsmud/amudsmud.github.io/releases/download/V1.0-beta/SHUMPEXE.EXE", "for å laste ned spillet mitt", "Noen Virus-program vil se på dette som et virus, men det er det ikke.", "<i> V 0.1.0-beta <br> Sist oppdatert: 23. April 2018 </i>")
     nyboks("a", "Klikk her", "kenneth2.0.html", "for å åpne noing's fete sirkler", "Laget til: Noing", "laget av: en random kar")
