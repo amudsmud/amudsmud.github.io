fed3cc93234f4981d8df234225ad08035c50ed66
Amud mac <max1231@live.no>
2019-04-20 21:14:07 +0200

    <-

--- spill/javascript/car-town9.js
+++ spill/javascript/car-town9.js
@@ -74,28 +74,28 @@ function start_spill() {
     };
     for (var i=0; i < stickersrc.length; i++){
         load_min_sticker[i] = new Image();
-        load_min_sticker[i].src = stickersrc[i]
         load_min_sticker[i].onload = myFunctionn(load_min_sticker[i]);
+        load_min_sticker[i].src = stickersrc[i]
     };
     for (var i=0; i < load_stickersrc1_list.length; i++){
         load_stickersrc1[i] = new Image();
-        load_stickersrc1[i].src = load_stickersrc1_list[i];
         load_stickersrc1[i].onload = myFunctionn(load_stickersrc1[i]);
+        load_stickersrc1[i].src = load_stickersrc1_list[i];
     };
     for (var i=0; i < load_biler_list.length; i++){
         load_biler[i] = new Image();
-        load_biler[i].src = load_biler_list[i];
         load_biler[i].onload = myFunctionn(load_biler[i]);
+        load_biler[i].src = load_biler_list[i];
     };
     for (var i=0; i < load_keys_list.length; i++){
         load_keys[i] = new Image();
-        load_keys[i].src = load_keys_list[i];
         load_keys[i].onload = myFunctionn(load_keys[i]);
+        load_keys[i].src = load_keys_list[i];
     };
     for (var i=0; i < load_goldkeys_list.length; i++){
         load_goldkeys[i] = new Image();
-        load_goldkeys[i].src = load_goldkeys_list[i];
         load_goldkeys[i].onload = myFunctionn(load_goldkeys[i]);
+        load_goldkeys[i].src = load_goldkeys_list[i];
     };
     load_bakgrunn.src = "bilder/map_bilder/map1.png";
     load_bakgrunn.onload = function(){
