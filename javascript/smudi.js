aa2a0f46e67afbd09ccaa8f6c63fdd3f90ed6a08
Amud mac <max1231@live.no>
2019-09-16 11:49:26 +0200

    fixed some duplicated in css and removed unused code

--- javascript/smudi.js
+++ javascript/smudi.js
@@ -63,9 +63,7 @@ var lastScrollTop = 0;
 var delta = 10;
 var navbarHeight = $('header').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
-});
+
 
 setInterval(function() {
     if (didScroll) {
